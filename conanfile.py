57ef27e2cb0df0ada42e92c9c28eb315187052ab
laundrevity <mahany25@gmail.com>
2022-07-15 13:35:30 +0200

    fix

--- conanfile.py
+++ conanfile.py
@@ -3,5 +3,10 @@ from conans import ConanFile, CMake
 
 class BoostShmemConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "boost/1.55.0"
+    requires = "boost/1.78.0"
     generators = "cmake", "gcc", "txt"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
